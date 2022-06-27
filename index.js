bb331af936b6d4cd3e17b2c630848bc6471fa042
Rajat Verma <rvrajatverma773@gmail.com>
2022-06-27 17:06:09 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-
-var num1="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+var n1=Math.floor((Math.random() * 6) + 1);
+var num1="images/dice"+n1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",num1);
 
-var num2="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+var n2=Math.floor((Math.random() * 6) + 1);
+var num2="images/dice"+n2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",num2);
 
-var num3="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+var n3=Math.floor((Math.random() * 6) + 1);
+var num3="images/dice"+n3+".png";
 document.querySelectorAll("img")[2].setAttribute("src",num3);
 
-var num4="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+var n4=Math.floor((Math.random() * 6) + 1);
+var num4="images/dice"+n4+".png";
 document.querySelectorAll("img")[3].setAttribute("src",num4);
