4342a029e61a16e804c0bd385546cda460aec29e
Rajat Verma <rvrajatverma773@gmail.com>
2022-06-27 16:40:03 +0530

    fix visual
    fixed look and style of the page

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
-var n1=Math.floor((Math.random() * 6) + 1);
-var num1="images/dice"+n1+".png";
+var num1="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
 document.querySelectorAll("img")[0].setAttribute("src",num1);
 
-var randomNumber1=Math.floor(Math.random()*6)+1;        // 1-6
-var randomDiceImage="dice"+randomNumber1+".png";         //dice1.png dice6.png
-var randomImageSource="images/"+randomDiceImage;        //images/dicel.png images/did
-var image1=document.querySelectorAll("img")[1];
-image1.setAttribute("src",randomImageSource);
+var num2="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+document.querySelectorAll("img")[1].setAttribute("src",num2);
 
+var num3="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+document.querySelectorAll("img")[2].setAttribute("src",num3);
 
- 
+var num4="images/dice"+(Math.floor((Math.random() * 6) + 1))+".png";
+document.querySelectorAll("img")[3].setAttribute("src",num4);
